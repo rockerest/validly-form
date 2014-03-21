0cb0d11b682f6cbb917906450717b4d26bb1e8f9
rockerest <tom@vu>
2014-03-21 17:23:36 -0500

    Adding dummy tests

---
+++ test/validly-form.js
@@ -0,0 +1,36 @@
+define(
+    ["validly-form"],
+    function( Validly ){
+        describe( "Validly.form", function(){
+            var iv,form;
+
+            beforeEach( function(){
+                iv = new Validly();
+                password = iv.password;
+            });
+
+            it.skip( "should return an object", function(){
+            });
+
+            describe.skip( "#getFieldsToValidate", function(){
+                it();
+            });
+
+            describe.skip( "#manageField", function(){
+                it();
+            });
+
+            describe.skip( "#validateField", function(){
+                it();
+            });
+
+            describe.skip( "#validatePassword", function(){
+                it();
+            });
+
+            describe.skip( "#testPasswordStrength", function(){
+                it();
+            });
+        });
+    }
+);
