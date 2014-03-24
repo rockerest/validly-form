6fd77562e12adaa5e36155f00ee7d5e5886c8224
rockerest <tom@vu>
2014-03-24 17:23:40 -0500

    Adding some tests

--- test/validly-form.js
+++ test/validly-form.js
@@ -1,15 +1,27 @@
 define(
-    ["validly-form"],
-    function( Validly ){
+    ["validly-form", "chai"],
+    function( Validly, Chai ){
         describe( "Validly.form", function(){
-            var iv,form;
+            var expect = Chai.expect,
+                html = document.getElementById( "validly-form" ),
+                div = document.getElementById( "mocha" ),
+                iv,form;
 
             beforeEach( function(){
                 iv = new Validly();
-                password = iv.password;
+                form = new iv.form( html, {"autostart":false} );
             });
 
-            it.skip( "should return an object", function(){
+            it( "should return a constructor", function(){
+                iv.form.should.be.a( "function" );
+            });
+
+            it( "should throw an error when constructed with nothing", function(){
+                expect( function(){ new iv.form(); } ).to.throw( Error );
+            });
+
+            it( "should throw an error when constructed with something other than a FORM tag", function(){
+                expect( function(){ new iv.form( div ); } ).to.throw( Error );
             });
 
             describe.skip( "#getFieldsToValidate", function(){
@@ -30,6 +42,30 @@ define(
 
             describe.skip( "#testPasswordStrength", function(){
                 it();
+            });
+
+            describe( "#start", function(){
+                beforeEach( function(){
+                    sinon.spy( form, "getFieldsToValidate" );
+                    sinon.spy( form, "manageField" );
+                });
+
+                afterEach( function(){
+                    form.getFieldsToValidate.restore();
+                    form.manageField.restore();
+                });
+
+                it( "should call #getFieldsToValidate once", function(){
+                    form.start();
+
+                    form.getFieldsToValidate.should.have.been.calledOnce;
+                });
+
+                it( "should call #manageField the correct number of times", function(){
+                    form.start();
+
+                    form.manageField.callCount.should.equal( 5 ); // number of `data-validly="aware"` fields
+                });
             });
         });
     }
