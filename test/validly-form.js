09eb550955d1383cbe1c2f9604619f0f830aeddf
rockerest <tom@vu>
2014-03-25 17:47:12 -0500

    Finish tests

--- test/validly-form.js
+++ test/validly-form.js
@@ -24,30 +24,166 @@ define(
                 expect( function(){ new iv.form( div ); } ).to.throw( Error );
             });
 
-            describe.skip( "#getFieldsToValidate", function(){
-                it();
+            describe( "#getFieldsToValidate", function(){
+                it( "should return an array", function(){
+                    form.getFieldsToValidate().should.be.a( "array" );
+                });
+
+                it( "should return an array of every node in the form to be processed by Validly", function(){
+                    form.getFieldsToValidate().length.should.equal( 6 );
+                });
+
+                it( "should push the HTML DomNode directly onto the array", function(){
+                    form.getFieldsToValidate()[5].should.equal( document.getElementById( "penguinInput" ) );
+                });
             });
 
-            describe.skip( "#manageField", function(){
-                it();
+            describe( "#manageField", function(){
+                it( "should add an event listener on keyup to the node", function(){
+                    sinon.spy( div, "addEventListener" );
+                    form.manageField( div );
+
+                    div.addEventListener.should.have.been.calledWith( "keyup" );
+                });
+
+                it( "should throw an error when passed something that is not an instance of EventTarget", function(){
+                    expect( function(){ form.manageField( "string" ); } ).to.throw( Error );
+                });
             });
 
-            describe.skip( "#validateField", function(){
-                it();
+            describe( "#validateField", function(){
+                var text = document.getElementById( "penguinInput" ),
+                    pass = document.getElementById( "passwordInput" );
+
+                beforeEach(function(){
+                    text.value = "";
+                    pass.value = "";
+                });
+
+                describe( "when passed a non-password field", function(){
+                    it( "should call the pass handler when the field passes validation", function(){
+                        text.value = "penguin";
+                        sinon.spy( form.options.handlers, "pass" );
+
+                        form.validateField( text, 1 );
+
+                        form.options.handlers.pass.should.have.been.calledOnce;
+                    });
+
+                    it( "should call the fail handler when the field fails validation", function(){
+                        text.value = "piguin";
+                        sinon.spy( form.options.handlers, "fail" );
+
+                        form.validateField( text, 1 );
+
+                        form.options.handlers.fail.should.have.been.calledOnce;
+                    });
+                });
+
+                describe( "when passed a password field", function(){
+                    it( "should make a call to #validatePassword", function(){
+                        sinon.spy( form, "validatePassword" );
+
+                        form.validateField( pass );
+
+                        form.validatePassword.should.have.been.calledOnce;
+                    });
+                });
             });
 
-            describe.skip( "#validatePassword", function(){
-                it();
+            describe( "#validatePassword", function(){
+                var pass = document.getElementById( "passwordInput" ),
+                    strg = document.getElementById( "passwordStrengthInput" );
+
+                beforeEach(function(){
+                    pass.value = "";
+                    strg.value = "";
+                    form.validator.password.filters = [];
+
+                    sinon.spy( form, "testPasswordStrength" );
+                    sinon.spy( form.validator.password, "meetsMinimumFilters" );
+                    sinon.spy( form.options.handlers.password, "pass" );
+                    sinon.spy( form.options.handlers.password, "fail" );
+                });
+
+                afterEach(function(){
+                    form.testPasswordStrength.restore();
+                    form.validator.password.meetsMinimumFilters.restore();
+                    form.options.handlers.password.pass.restore();
+                    form.options.handlers.password.fail.restore();
+                });
+
+                describe( "when the `strength` trigger is present", function(){
+                    it( "should make a call to #testPasswordStrength", function(){
+                        form.validatePassword( strg );
+
+                        form.testPasswordStrength.should.have.been.calledOnce;
+                    });
+                });
+
+                describe( "when the `strength` trigger is not present", function(){
+                    it( "should not make a call to #testPasswordStrength", function(){
+                        form.validatePassword( pass );
+
+                        form.testPasswordStrength.should.not.have.been.called;
+                    });
+                });
+
+                it( "should make a call to password#meetsMinimumFilters", function(){
+                    form.validatePassword( pass );
+
+                    form.validator.password.meetsMinimumFilters.should.have.been.calledOnce;
+                });
+
+                it( "should call the password pass handler if the password passes validation", function(){
+                    pass.value = "Ab1";
+
+                    form.validatePassword( pass, 1 );
+
+                    form.options.handlers.password.pass.should.have.been.calledOnce;
+                });
+
+                it( "should call the password fail handler is the password fails validation", function(){
+                    pass.value = "password";
+
+                    form.validatePassword( pass, 1 );
+
+                    form.options.handlers.password.fail.should.have.been.calledOnce;
+                });
             });
 
-            describe.skip( "#testPasswordStrength", function(){
-                it();
+            describe( "#testPasswordStrength", function(){
+                var pass = document.getElementById( "passwordInput" ),
+                    str, test;
+
+                before(function(){
+                    str = sinon.spy( form.options.handlers.password, "strength" );
+                    test = sinon.spy( form.validator.password, "testStrength" );
+                    form.testPasswordStrength( pass, 1 );
+                });
+
+                after(function(){
+                    str.restore();
+                    test.restore();
+                });
+
+                it( "should make a call to password#testStrength", function(){
+                    test.should.have.been.calledOnce;
+                });
+
+                it( "should make a call to the password strength handler", function(){
+                    str.should.have.been.calledOnce;
+                });
+
+                it( "should call password#testStrength before the password strength handler", function(){
+                    test.calledBefore( str );
+                });
             });
 
             describe( "#start", function(){
                 beforeEach( function(){
                     sinon.spy( form, "getFieldsToValidate" );
-                    sinon.spy( form, "manageField" );
+                    sinon.stub( form, "manageField", function(){} );
                 });
 
                 afterEach( function(){
@@ -64,7 +200,8 @@ define(
                 it( "should call #manageField the correct number of times", function(){
                     form.start();
 
-                    form.manageField.callCount.should.equal( 5 ); // number of `data-validly="aware"` fields
+                    // number of `data-validly="aware"` fields
+                    form.manageField.callCount.should.equal( 6 );
                 });
             });
         });
